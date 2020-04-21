f3201580b8a9ce80e1623a8636d587ebcc98b22c
vladislav070 <shramenko.vlad@gmail.com>
2020-04-21 17:35:14 +0300

    Application icon added. Created executable file for Windows. Changed the version of SDK from 11 to 8.

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import utilities.I18N;
 
@@ -14,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.titleProperty().bind(I18N.createStringBinding("title"));
+        primaryStage.getIcons().add(new Image("/res/icon.png"));
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
         ResourceBundle bundle = ResourceBundle.getBundle("messages", I18N.getLocale());
         fxmlLoader.setResources(bundle);
